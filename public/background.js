bc0d653177522585a18b21700ef6ac9e874afbf8
Yuxiang Dong <yuxiang.dong@stonybrook.edu>
2024-02-10 10:29:41 -0500

    The gemini now works

--- public/background.js
+++ public/background.js
@@ -66,27 +66,6 @@ chrome.runtime.onMessage.addListener(
 ); 
 */
 
-// import { GoogleGenerativeAI } from "@google/generative-ai";
-
-// // Access your API key (see "Set up your API key" above)
-// const genAI = new GoogleGenerativeAI(API_KEY);
-
-// async function run() {
-//   // For text-only input, use the gemini-pro model
-//   const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-
-//   const prompt = "Write a story about a magic backpack."
-
-//   const result = await model.generateContent(prompt);
-//   const response = await result.response;
-//   const text = response.text();
-//   console.log(text);
-// }
-
-// run();
-
-
-
 //create AskGPT context manual
 chrome.runtime.onInstalled.addListener(async () => {
       chrome.contextMenus.create({
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-
+import run from "./api/geminiAPI";
 
 function App() {
+  run();
+
   return (
     <div className="App text-4xl font-bold">
       <h1>Hello extension from react</h1>
